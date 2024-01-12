ed409c866a92af4d5e7ec023966e70f865dbda2a
Ashfmate <ashforestunknown@gmail.com>
2024-01-12 11:04:48 +0300

    Working now. finally, I only have to add a struct, implement the subcmd trait and add it into the vector, muuuuch simpler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
+use std::ops::Deref;
+
+use clap::{
+	Command,
+	builder::styling::{
+		Styles,
+		AnsiColor,
+		Effects},
+	ArgMatches};
+
+mod sub_commands;
+use colored::Colorize;
+use sub_commands::*;
+
 fn main() {
+	let cmds:Vec<Box<dyn SubCmd>> = vec![
+		Box::new(Init::new("init")),
+		];
+
+	let matches = kitpp(&cmds).get_matches();
+
+	process_matches(cmds,matches);
+}
+
+fn kitpp(cmds: &Vec<Box<dyn SubCmd>>) -> Command {
+	Command::new("kit++")
+		.about("C++ Project and package manager")
+		.version("0.1.0")
+		.subcommand_required(true)
+		.arg_required_else_help(true)
+		.author("Ashfmate - Git / Ashforest everything else")
+		.styles(
+			Styles::styled()
+			.header(AnsiColor::Green.on_default() | Effects::BOLD))
+		.subcommands(
+			cmds
+				.iter()
+				.map(|item| item.deref().cmd())
+			)
+}
+
+fn process_matches(cmds: Vec<Box<dyn SubCmd>>,matches: ArgMatches) {
+	let (name,matcher) = matches
+		.subcommand()
+		.expect("There should be a subcommand\nThis error shouldn't appear");
 	
+	let cmd = cmds
+		.into_iter()
+		.find(|item| item.get_name() == name);
+
+	if let Some(mut cmd) = cmd {
+		cmd.fill_matcher(matcher.clone());
+		cmd.process();
+	} else {
+		eprintln!("{} {}",
+			name.red(),
+			"does not exist as a subcommand".red());
+	}
 }
